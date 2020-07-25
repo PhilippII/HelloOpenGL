05832647ead48906bead4f205cc0c4f267c39f54
Philipp Schoch <9418998+PhilippII@users.noreply.github.com>
2020-07-25 23:27:25 +0200

    add functions for compiling and linking vertex and fragment shader

--- src/main.cxx
+++ src/main.cxx
@@ -3,6 +3,39 @@
 
 #include <iostream>
 
+static unsigned int compileShader(unsigned int type, const std::string& source) {
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    // TODO: error handling
+
+    return id;
+}
+
+/*
+ * - compile vertexShader
+ * - compile fragmentShader
+ * - link them both together into a single shader program
+ * - return a unique identifier to that shader program
+ */
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
+    unsigned int program = glCreateProgram();
+    unsigned int vs = compileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = compileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
 int main(void)
 {
     #ifdef DEBUG
