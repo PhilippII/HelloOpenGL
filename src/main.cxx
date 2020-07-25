7e15403859bea8ebcbf3756b8950696fed398349
Philipp Schoch <9418998+PhilippII@users.noreply.github.com>
2020-07-26 00:23:00 +0200

    add actual shader code and use it

--- src/main.cxx
+++ src/main.cxx
@@ -96,6 +96,28 @@ int main(void)
     glEnableVertexAttribArray(posAttrIndex);
     glVertexAttribPointer(posAttrIndex, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), 0);
 
+    std::string vertexShader =
+            "#version 330 core\n"
+            "\n"
+            "layout(location = 0) in vec4 position;\n"
+            "\n"
+            "void main()\n"
+            "{\n"
+            "   gl_Position = position;\n"
+            "}\n";
+    std::string fragmentShader =
+            "#version 330 core\n"
+            "\n"
+            "layout(location = 0) out vec4 color;\n"
+            "\n"
+            "void main()\n"
+            "{\n"
+            "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+            "}\n";
+
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
