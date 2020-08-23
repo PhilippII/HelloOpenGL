c736fa37597b9e9fc4f8e0237190261c3b74ad76
Philipp Schoch <9418998+PhilippII@users.noreply.github.com>
2020-08-23 21:02:25 +0200

    more meshes: - simple plane to see if counterclockwise order is preserved - asymmetrical version of suzanne (earbud on left side from suzanne's point of view) 	-> needed to see if the x coordinates are flipped 	due to mismatch between blenders and my coordinate system 	-> also save original blend file in addition to 		Wavefront OBJ-file

--- src/main.cxx
+++ src/main.cxx
@@ -185,7 +185,7 @@ int main(void)
 
     // import from OBJ-file:
     GLShaderProgram suzanneSP("res/shaders/Suzanne.shader");
-    CPUMesh<GLuint> suzanneCPUMesh = readOBJ("res/meshes/suzanne_scaled_smooth_subdiv_1_ndc.obj");
+    CPUMesh<GLuint> suzanneCPUMesh = readOBJ("res/meshes/suzanne_scaled_smooth_subdiv_1_left_earring.obj");
     suzanneCPUMesh.va.layout.setDefaultLocations(); // assumes vertex attributes
                                                     // were added to layout in order
                                                     // position, texCoordinate, normal
