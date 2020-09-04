2cd3c22ddfe8488f3c3d8ea0854a9b6d9a321173
Philipp Schoch <9418998+PhilippII@users.noreply.github.com>
2020-09-04 13:36:12 +0200

    pass filepath to loadOBJfile by reference

--- inc/cpu_mesh_import.h
+++ inc/cpu_mesh_import.h
@@ -4,6 +4,6 @@
 #include <filesystem>
 #include "cpu_mesh_structs.h" // for CPUMesh<T>
 
-std::vector<CPUMesh<GLuint>> loadOBJfile(std::filesystem::path filepath, bool invert_z = false);
+std::vector<CPUMesh<GLuint>> loadOBJfile(const std::filesystem::path& filepath, bool invert_z = false);
 
 #endif // CPU_MESH_IMPORT_H
