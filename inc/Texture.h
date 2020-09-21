4982bcce706441a3f723207d08190007769b418e
Philipp Schoch <9418998+PhilippII@users.noreply.github.com>
2020-09-21 17:42:00 +0200

    add new class Texture (does not really do anything yet.)

---
+++ inc/Texture.h
@@ -0,0 +1,24 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include "Renderer.h"
+
+class Texture
+{
+public:
+    Texture(const std::string& path);
+    ~Texture();
+
+    void Bind(unsigned int slot) const;
+    void Unbind() const;
+
+    int GetWidth() const { return m_Width; }
+    int GetHeight() const { return m_Height; }
+private:
+    unsigned int m_RendererID;
+    std::string m_FilePath;
+    unsigned char* m_LocalBuffer;
+    int m_Width, m_Height, m_BPP;
+};
+
+#endif // TEXTURE_H
