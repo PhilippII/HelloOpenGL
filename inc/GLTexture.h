e0eae6a24d17e76357a8eb98218a254b499b94a3
Philipp Schoch <9418998+PhilippII@users.noreply.github.com>
2020-09-26 18:21:54 +0200

    add helper function to generate a chess pattern as a texture image so the texture stuff can be tested before the actual png-loading is implemented

--- inc/GLTexture.h
+++ inc/GLTexture.h
@@ -4,6 +4,8 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <vector>
+
 class GLTexture
 {
 public:
@@ -11,6 +13,7 @@ public:
     ~GLTexture();
 private:
     static GLsizei computeMipLevelCount(GLsizei width, GLsizei height);
+    static std::vector<GLubyte> makeCheckerPattern(GLsizei& width, GLsizei& height);
     GLuint m_rendererId;
     GLsizei m_width;
     GLsizei m_height;
--- src/GLTexture.cxx
+++ src/GLTexture.cxx
@@ -19,6 +19,10 @@ GLTexture::GLTexture() : m_rendererId(0),
                    , m_width
                    , m_height);
 
+    std::vector<GLubyte> pix_data = makeCheckerPattern(m_width, m_height);
+    // glTexSubImage2D(..)
+    // glGenerateMipmap(..)
+
     // TODO:
     // 1) actually upload data to the allocated storage
     // 2) implement move/copy-constructor/assignment
@@ -39,4 +43,22 @@ GLsizei GLTexture::computeMipLevelCount(GLsizei width, GLsizei height)
         //           << (size >> level) << '\n';
     }
     return level; // return first level where (size >> level) would be zero
+}
+
+std::vector<GLubyte> GLTexture::makeCheckerPattern(GLsizei &width, GLsizei &height)
+{
+    std::array<std::vector<GLubyte>, 2> colors = {
+            std::vector<GLubyte>{  0,   0,   0},
+            std::vector<GLubyte>{255, 255, 255}
+        };
+    std::vector<GLubyte> res;
+    for (GLsizei y = 0; y < height; ++y) {
+        for (GLsizei x = 0; x < width; ++x) {
+            constexpr mask = (1 << 4);
+            int col_index = ((x & mask) ^ (y & mask) != 0) ? 1 : 0;
+            std::copy(colors[col_index].begin(), colors[col_index].end(),
+                      std::back_inserter(res));
+        }
+    }
+    return res;
 }
