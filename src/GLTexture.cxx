1ec0503e00ccdb391224e6be5a11407566cd2b9c
Philipp Schoch <9418998+PhilippII@users.noreply.github.com>
2020-09-26 21:35:37 +0200

    add forgotten GLCall() macros

--- src/GLTexture.cxx
+++ src/GLTexture.cxx
@@ -7,44 +7,44 @@ GLTexture::GLTexture() : m_rendererId(0),
                          m_width(256), m_height(256),
                          m_mipLevels(computeMipLevelCount(m_width, m_height)) // no mipmapping so far
 {
-    glGenTextures(1, &m_rendererId);
+    GLCall(glGenTextures(1, &m_rendererId));
 
     // first time bind also determines type of texture:
-    glBindTexture(GL_TEXTURE_2D, m_rendererId);
+    GLCall(glBindTexture(GL_TEXTURE_2D, m_rendererId));
 
     // allocate immutable storage:
     //  (immutable = immutable-format but contents may still be modified)
-    glTexStorage2D(GL_TEXTURE_2D,
-                   m_mipLevels,
-                   GL_RGB8, // guaranteed to be supported (socalled required format)
-                            // for textures but not guaranteed for renderbuffers
-                   m_width,
-                   m_height);
+    GLCall(glTexStorage2D(GL_TEXTURE_2D,
+                          m_mipLevels,
+                          GL_RGB8, // guaranteed to be supported (socalled required format)
+                                   // for textures but not guaranteed for renderbuffers
+                          m_width,
+                          m_height));
 
     // upload actual contents of the texture:
     std::vector<GLubyte> pix_data = makeCheckerPattern(m_width, m_height);
-    glTexSubImage2D(GL_TEXTURE_2D,
-                    0, // lod-level
-                    0, 0, // x, y-offset
-                    m_width, m_height,
-                    GL_RGB, GL_UNSIGNED_BYTE,
-                    pix_data.data());
+    GLCall(glTexSubImage2D(GL_TEXTURE_2D,
+                           0, // lod-level
+                           0, 0, // x, y-offset
+                           m_width, m_height,
+                           GL_RGB, GL_UNSIGNED_BYTE,
+                           pix_data.data()));
 
     // generate content of other mipmap levels:
-    glGenerateMipmap(GL_TEXTURE_2D);
+    GLCall(glGenerateMipmap(GL_TEXTURE_2D));
 
     // set texture parameters:
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
+    GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR));
+    GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT));
+    GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT));
 
     // TODO: implement move/copy-constructor/assignment
 }
 
 GLTexture::~GLTexture()
 {
-    glDeleteTextures(1, &m_rendererId); // docs.gl: "glDeleteTextures(..) silently ignores 0's [...]"
+    GLCall(glDeleteTextures(1, &m_rendererId)); // docs.gl: "glDeleteTextures(..) silently ignores 0's [...]"
 }
 
 
