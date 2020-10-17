8c76b666548e65a32f5b1dcd7df43b4c73974de0
Philipp Schoch <9418998+PhilippII@users.noreply.github.com>
2020-10-17 22:45:37 +0200

    add class DemoSuite which allows to combine multiple demos into one ("master-")demo. This "master-demo" will show a menu, which allows to switch between the different "child-demos".

--- inc/demos/Demo.h
+++ inc/demos/Demo.h
@@ -1,6 +1,11 @@
 #ifndef DEMO_H
 #define DEMO_H
 
+#include <vector>
+#include <string>
+#include <functional>
+#include <memory> // for std::unique_ptr<..>
+
 #include "GLRenderer.h"
 
 namespace demo {
@@ -21,6 +26,26 @@ public:
     GLRenderer& getRenderer() { return m_renderer; }
 private:
     GLRenderer& m_renderer;
+};
+
+class DemoSuite : public Demo {
+public:
+    DemoSuite(GLRenderer& renderer);
+    ~DemoSuite();
+
+    void OnUpdate(float deltaTime) override;
+    void OnRender() override;
+    void OnImGuiRender() override;
+
+    template<typename T>
+    void RegisterDemo(std::string name) {
+        m_demos.push_back(std::pair(std::move(name),
+                                    [](GLRenderer& renderer){ return std::make_unique<T>(renderer); }
+                                   ));
+    }
+private:
+    std::unique_ptr<Demo> m_currentDemo;
+    std::vector<std::pair<std::string, std::function<std::unique_ptr<Demo>(GLRenderer&)>>> m_demos;
 };
 
 }
--- src/demos/Demo.cxx
+++ src/demos/Demo.cxx
@@ -1,3 +1,51 @@
 #include "demos/Demo.h"
+#include "imgui.h"
 
+namespace demo {
 
+DemoSuite::DemoSuite(GLRenderer &renderer)
+    : Demo(renderer)
+{
+    getRenderer().setClearColor();
+}
+
+DemoSuite::~DemoSuite()
+{
+
+}
+
+void DemoSuite::OnUpdate(float deltaTime)
+{
+    if (m_currentDemo) {
+        m_currentDemo->OnUpdate(deltaTime);
+    }
+}
+
+void DemoSuite::OnRender()
+{
+    if (m_currentDemo) {
+        m_currentDemo->OnRender();
+    } else {
+        getRenderer().clear(GL_COLOR_BUFFER_BIT);
+    }
+}
+
+void DemoSuite::OnImGuiRender()
+{
+    if (m_currentDemo) {
+        if (ImGui::Button("<-")) {
+            m_currentDemo.reset();
+            getRenderer().setClearColor();
+        } else {
+            m_currentDemo->OnImGuiRender();
+        }
+    } else {
+        for (auto& p : m_demos) {
+            if (ImGui::Button(p.first.c_str())) {
+                m_currentDemo = p.second(getRenderer());
+            }
+        }
+    }
+}
+
+}
