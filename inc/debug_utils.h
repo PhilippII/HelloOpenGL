6d24e9462409c1e1228c4f341347c632b5708c0d
philipp <9418998+PhilippII@users.noreply.github.com>
2020-11-12 23:26:57 +0100

    on windows myAssert(x) uses __debugbreak() 	instead of raise(SIGTRAP)

--- inc/debug_utils.h
+++ inc/debug_utils.h
@@ -1,12 +1,18 @@
 #ifndef DEBUG_UTILS_H
 #define DEBUG_UTILS_H
 
+#ifndef WIN32
 #include <signal.h> // for SIGTRAP in myAssert
+#endif
 
 #ifdef NDEBUG
 #define myAssert(x)
 #else
+#ifdef WIN32
+#define myAssert(x) if (!(x)) __debugbreak()
+#else
 #define myAssert(x) if (!(x)) raise(SIGTRAP)
+#endif
 #endif
 
 #ifdef NDEBUG
